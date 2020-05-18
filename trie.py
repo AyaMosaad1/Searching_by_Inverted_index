5154773c9a2c3755f1f9fb1bbc2897e5f8eba247
AlaaMohamed99 <57914350+AlaaMohamed99@users.noreply.github.com>
2020-05-18 16:29:04 +0200

    works on all but japenese letters

--- trie.py
+++ trie.py
@@ -33,9 +33,10 @@ class Trie:
 
         for i in range(length):
             index = ord(word[i]) - ord('a')
-            if not node.child[index]:
-                node.child[index] = TrieNode()
-            node = node.child[index]
+            if 0 <= index <= 26:
+                if not node.child[index]:
+                    node.child[index] = TrieNode()
+                node = node.child[index]
         node.end.add(doc)
 
     #
@@ -73,25 +74,24 @@ class Trie:
 #
 
 
-def fileToList():
-    entries = os.listdir('D:/DATA STRUCTURE/textfiles')
-    for entry in entries:
-        print(entry)
-        f = open(entry, "r")
-        s = f.readline()
-        keys = list(map(str, s.strip().split('.')))[:len(s)]
-        return keys
-
-def filesread():
-    for path in pathlib.Path("D:/DATA STRUCTURE/test").iterdir():
-
+def readallfiles(p, hello):
+    for path in pathlib.Path(p).iterdir():
         if path.is_file():
             current_file = open(path, "r", encoding='UTF-8')
             s = current_file.read()
-            keys = list(map(str, s.strip().split()))[:len(s)]
+            import re
+            # D, file, anotherfile, name = path.split('/')
+            keys = list(map(str, re.split('[@. ]', s)))[:len(s)]
+            for key in keys:
+                if key.find("’") != -1:
+                    x = key.find("’")  # ’ mo5tlfa 3an '
+                    key = key[:x]
+                else:
+                    key = ''.join([i for i in key if i.isalpha()])
+                hello.insert(key, path.name)
             current_file.close()
-            # print(current_file.read())
-    return keys
+
+
 
 
 #
@@ -100,35 +100,11 @@ def filesread():
 #       @return  this var
 #
 #
-
+#"D:/DATA STRUCTURE/questions"
 
 T = Trie()
-#no such file or directory
-#entries = os.listdir('D:/DATA STRUCTURE/textfiles')
-#for entry in entries:
-#       print(entry)
-#       f = open(entry, "r")
-#       s = f.readline()
-#       keys = list(map(str, s.strip().split()))[:len(s)]
-#       for key in keys:
-#           T.insert(key, entry)
-#       f.close()
+readallfiles("D:/DATA STRUCTURE/textfiles", T)
 
-for path in pathlib.Path("D:/DATA STRUCTURE/textfiles").iterdir():
-    if path.is_file():
-        current_file = open(path, "r", encoding='UTF-8')
-        s = current_file.read()
-        import re
-        #D, file, anotherfile, name = path.split('/')
-        keys = list(map(str, re.split('[@. ]',s)))[:len(s)]
-        for key in keys:
-            if key.find("’") != -1:
-                x = key.find("’")  # ’ mo5tlfa 3an '
-                key = key[:x]
-            else:
-                key = ''.join([i for i in key if i.isalpha()])
-            T.insert(key, path.name)
-        current_file.close()
 
 #words = filesread()
 #print(words)
@@ -142,6 +118,6 @@ for path in pathlib.Path("D:/DATA STRUCTURE/textfiles").iterdir():
 # T.insert("sara", "first")
 # T.insert("Sara", "second")
 
-L = T.search("master")
-
+L = T.search("this")
+#D = T.search("this")
 print(L)
