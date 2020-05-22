1d2b7c98efbc2cd4c2f7485e84671a53a4508ed3
panse <47269934+panse98@users.noreply.github.com>
2020-05-22 07:53:10 +0200

    Add files via upload

--- DS.py
+++ DS.py
@@ -5,32 +5,38 @@
 #tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 import re
 import string
+import os
+
 
 #for line in file
 
+
 with open("E:\DS Project\dsdoc.txt","r") as file:
 
- #dh 3chan mytb3sh \n bs
-   string_without_line_breaks = ""
-   for line in file:
+  data=file.read()
+
+# clean = open('E:\DS Project\dsdoc.txt').read().replace('\n', '')
+'''string_without_line_breaks = ""
+for line in file:
     stripped_line = line.rstrip()
     string_without_line_breaks += stripped_line
     #string_without_line_breaks = string_without_line_breaks.replace(',' , "  ")
-
+'''
 file.close()
 
-word="pansea"
+word="roses"
 
-result=(re.findall(r"[^.]*?"+(word)+r"[^.]*.", string_without_line_breaks))
+result={}
+#hnbdel asm l file b variable 3chan a7ot ay file
 
+result=(re.findall(r"[^\n]*?"+(word.lower())+r"[^\n]*\n", data))
 
-#for i in string_without_line_breaks:
-# if (string_without_line_breaks[(len(word)+1)]== 'a'):
+'''for i in result:
+    print(i)
+'''
+final=[]
+for i in result:
+  final.append((i.strip()))
 
-
-#result=(re.findall(r"[^.]*?\S"+(word.lower())+r"[^.]*\.S", string_without_line_breaks))
-
-
-
-print(result)
+print(final)
 
