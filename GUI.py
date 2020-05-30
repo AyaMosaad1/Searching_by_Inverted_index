0b2fc8d7b2d19bd822aee064e7a270444a192614
baraamagdy <baraamagdy2004@gmail.com>
2020-05-30 04:57:31 +0200

    DisplayFile(self)-need modification

--- GUI.py
+++ GUI.py
@@ -1,6 +1,9 @@
 import sys
 import time
 import os.path
+
+from PyQt5.QtCore import QRegExp
+
 from trie import *
 from pathlib import Path
 from PyQt5.QtGui import *
@@ -8,10 +11,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 
-
 T = Trie()
-output = list()
-fileName = " "
+
 # -----------------------------------initialize a window-----------------------
 class Example(QWidget):
 
@@ -22,20 +23,20 @@ class Example(QWidget):
     def initUI(self):
 
         # ----------------main window-----------------
-        self.setGeometry(200, 100, 1000, 470)
+        self.setGeometry(140, 120, 1040, 490)
         self.setStyleSheet('background-color: rgb(255,255,255)')
         self.setWindowTitle('Searching_Engine')
         self.setWindowIcon(QtGui.QIcon('search.png'))
 
         # ----------------------- "image"-select folder  -----------------------------
         self.label = QLabel(self)
-        self.label.setGeometry(60, 80, 170, 170)
+        self.label.setGeometry(70, 90, 170, 170)
         pixmap = QPixmap('fin.png')
         self.label.setPixmap(pixmap)
 
         # -----------------------input text area for enter the searching word -----------------------------
         self.b = QPlainTextEdit(self)
-        self.b.setGeometry(305, 200, 210, 36)
+        self.b.setGeometry(320, 210, 200, 36)
         self.b.setStyleSheet("background-color:rgb(255,255,255);\n"
                              "color: rgb(33,36,45);\n"
                              "border-style: outset;\n"
@@ -47,11 +48,9 @@ class Example(QWidget):
         self.b.textChanged.connect(self.getText)
         self.b.setPlaceholderText("Enter the word")
 
-
-
         # ----------------info "Browse one step" button-----------------
         self.info = QPushButton('Browse folder', self)
-        self.info.setGeometry(30, 260, 220, 38)
+        self.info.setGeometry(40, 270, 220, 38)
         self.info.setFont(QFont('Arial', 10))
         self.info.setStyleSheet("background-color:rgb(33,36,45);\n"
                                 "color: white;\n"
@@ -64,7 +63,7 @@ class Example(QWidget):
 
         # -----------------Search button-----------------
         self.runB = QPushButton('Search', self)
-        self.runB.setGeometry(300, 260, 220, 38)
+        self.runB.setGeometry(310, 270, 220, 38)
         self.runB.setFont(QFont('Arial', 10))
         self.runB.setStyleSheet("background-color:rgb(33,36,45);\n"
                                 "color: white;\n"
@@ -73,15 +72,16 @@ class Example(QWidget):
                                 "border-radius:5px;\n"
                                 "padding :6px;\n"
                                 "min-width:10px;\n")
-        # when run it
         self.runB.clicked.connect(self.run)
 
-
-
-        #display output files
+        # -----------------  display output files -----------------
         self.listWidget = QListWidget(self)
-        self.listWidget.setGeometry(QtCore.QRect(560, 20, 400, 400))
-        self.listWidget.setStyleSheet("background-color:rgb(255,255,255);\n")
+        self.listWidget.setGeometry(QtCore.QRect(580, 20, 410, 430))
+        self.listWidget.setStyleSheet("background-color:rgb(255,255,255);\n"
+                                      "border-style: outset;\n"
+                                      "border-radius:5px;\n"
+                                      "border-width:1px;\n"
+                                      "border-color:rgb(33,36,45);\n")
         self.listWidget.itemClicked.connect(self.Clicked)
         # -----------------Progress widget-----------------
         # main widget
@@ -110,12 +110,48 @@ class Example(QWidget):
                                          "border: 0px solid ;\n")
         self.progressWidget.hide()
 
-        # ---------------------display file content--------------------------------
+        # -----------------choose output display widget-----------------
+        self.chooseStyleWidget = QtWidgets.QWidget(self)
+        self.chooseStyleWidget.setGeometry(QtCore.QRect(300, 135, 400, 180))
+        self.chooseStyleWidget.setObjectName("progressWidget")
+        self.chooseStyleWidget.setStyleSheet("background-color: white;\n"
+                                             "border-radius:5px;\n"
+                                             "border: 1px solid ;\n"
+                                             "border-color:rgb(33,36,45);\n")
 
+        self.choose = QLabel('Please choose one .......',self.chooseStyleWidget)
+        self.choose.setFont(QFont('Arial', 13))
+        self.choose.setGeometry(120, 30, 200, 70)
+        self.choose.setStyleSheet("border: 0px solid ;\n")
+
+
+        self.btnFile = QPushButton('Open file',self.chooseStyleWidget)
+        self.btnFile.setFont(QFont('Arial', 10))
+        self.btnFile.setGeometry(30, 100, 140, 35)
+        self.btnFile.setStyleSheet('background-color: white;\n'
+                                   "border-radius:5px;\n"
+                                   "border: 1px solid ;\n"
+                                   "border-color:rgb(33,36,45);\n")
+        self.btnFile.clicked.connect(self.DisplayFile)
+
+
+        self.btnSent = QPushButton('Show sentence',self.chooseStyleWidget)
+        self.btnSent.setGeometry(210, 100, 140, 35)
+        self.btnSent.setFont(QFont('Arial', 10))
+        self.btnSent.setStyleSheet("background-color: white;\n"
+                                   "border: 1px solid ;\n"
+                                   "border-radius:5px;\n"
+                                   "border-color:rgb(33,36,45);\n")
+        self.btnSent.clicked.connect(self.showSentence)
+
+        self.chooseStyleWidget.hide()
+
+        # ---------------------display file content--------------------------------
         self.fileWidget = QtWidgets.QWidget(self)
         self.fileWidget.setGeometry(QtCore.QRect(300, 120, 450, 240))
         self.fileWidget.setObjectName("fileWidget")
         self.fileWidget.setStyleSheet('background-color: white;\n'
+                                          "border-radius:5px;\n"
                                           "border: 1px solid ;\n"
                                           "border-color:rgb(33,36,45);\n")
         self.btnclose = QPushButton(self.fileWidget)
@@ -129,12 +165,7 @@ class Example(QWidget):
         self.closepic.setPixmap(pixmap)
         self.btnclose.clicked.connect(self.ClickedClose)
 
-        self.out = QPlainTextEdit(self.fileWidget)
-        self.out.setGeometry(QtCore.QRect(4, 24, 440, 200))
-        self.out.textChanged.connect(self.getText)
-        self.out.setReadOnly(True)
-        self.out.setStyleSheet("background-color:rgb(255,255,255);"
-                               "border: 0px solid ;\n" )
+
         self.fileWidget.hide()
 
         self.show()
@@ -144,12 +175,12 @@ class Example(QWidget):
         self.fileWidget.hide()
 
     def showProgress(self):
-        #show progress widget
+        # show progress widget
         self.progressWidget.show()
         self.prog.appendPlainText(str(0))
         QApplication.processEvents()
 
-        #loading data in files
+        # loading data in files
         i = 0
         for path in pathlib.Path(fileName).iterdir():
             if path.is_file():
@@ -178,34 +209,63 @@ class Example(QWidget):
 
         self.progressWidget.close()
 
+
     # return file name
     def Clicked(self, item):
         file_subPath = (item.text())
-        name=pathlib.Path(fileName, file_subPath)
+        global filePath
+        filePath=pathlib.Path(fileName, file_subPath)
+        self.chooseStyleWidget.show()
 
-        file = open(name, 'r')
 
-        with file:
-            text = file.read()
-            self.out.setPlainText(text)
+    def DisplayFile(self):
+        self.chooseStyleWidget.hide()
+        self.out = QPlainTextEdit(self.fileWidget)
+        self.out.setGeometry(QtCore.QRect(4, 24, 440, 200))
+        self.out.textChanged.connect(self.getText)
+        self.out.setReadOnly(True)
+        self.out.setStyleSheet("background-color:rgb(255,255,255);"
+                               "border: 0px solid ;\n")
+        self.out.show()
+
+        self.fileWidget.show()
+
+    def showSentence(self):
+        self.chooseStyleWidget.hide()
+        self.out = QPlainTextEdit(self.fileWidget)
+        self.out.setGeometry(QtCore.QRect(4, 24, 440, 200))
+        self.out.textChanged.connect(self.getText)
+        self.out.setReadOnly(True)
+        self.out.setStyleSheet("background-color:rgb(255,255,255);"
+                                 "border: 0px solid ;\n" )
+        with open(filePath, "r") as file:
+
+            d = file.readlines()
+            for line in d:
+                if word.lower() in line.lower():
+                    self.out.setPlainText(line)
+                    break
         file.close()
         self.fileWidget.show()
 
     # -------------------write txt from gui"we input the word throw input.txt to code" --------------------
     def getText(self):
-        word = self.b.document().toPlainText()
         global output
+        global word
+        word=self.b.document().toPlainText()
         output = T.search(word)
 
     # -------------------------------write the output into gui------------------------------------------
+
+
 
     def write(self):
         if output == "Not Found":
             self.listWidget.clear()
             self.listWidget.addItem(output)
         else:
-            #filesList = list(output)
-            #filesList = output
+            # filesList = list(output)
+            # filesList = output
             self.listWidget.clear()
             for file in output:
                 listWidgetItem = QListWidgetItem(file)
